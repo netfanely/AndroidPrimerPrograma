626c7cb27db538ce1914268d266d88a79ded969e
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2019-01-12 10:39:37 -0200

    Add files via upload

---
+++ app/src/main/java/br/ufg/inf/teste/MainActivity.java
@@ -0,0 +1,24 @@
+package br.ufg.inf.teste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+//import android.app.Activity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      //  setContentView(R.layout.activity_main);
+
+
+    }
+  /*  public void Cambia_Texto(View view){
+        //Boton
+        TextView texto = (TextView) findViewById(R.id.texto);
+        //elemento TextView
+        texto.setText("Cambia Texto Succesfull!!");
+    }*/
+}
