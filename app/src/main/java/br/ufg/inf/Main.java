5ea8d54896c5e3abcd974b4ea6da0328ce359da3
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2019-09-18 10:36:14 -0500

    Create Main.java
    Activity Principal

---
+++ app/src/main/java/br/ufg/inf/Main.java
@@ -0,0 +1,56 @@
+package com.netfanely.erlifegov02;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText user, pass;
+    Button btnEntrar, btnRegistrar;
+    daoUsuario dao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        user=(EditText)findViewById(R.id.User);
+        pass=(EditText)findViewById(R.id.Pass);
+        btnEntrar=(Button)findViewById(R.id.btnEntrar);
+        btnRegistrar=(Button)findViewById(R.id.btnRegistrar);
+        btnEntrar.setOnClickListener(this);
+        btnRegistrar.setOnClickListener(this);
+        dao=new daoUsuario(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnEntrar:
+                String u=user.getText().toString();
+                String p=pass.getText().toString();
+                if(u.equals("")&&p.equals("")){
+                    Toast.makeText(this, "ERROR: Campos vacios",Toast.LENGTH_LONG).show();
+                }else if(dao.login(u,p)==1){
+                    Usuario ux=dao.getUsuario(u,p);
+                    Toast.makeText(this, "Datos correctos",Toast.LENGTH_LONG).show();
+                    Intent i2=new Intent(MainActivity.this,Menu2Activity.class);
+                    i2.putExtra("Id",ux.getId());
+                    startActivity(i2);
+                    finish();
+                }else{
+                    Toast.makeText(this, "Usuario ou Senha errados",Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.btnRegistrar:
+                Intent i=new Intent(MainActivity.this,Registrar.class);
+                startActivity(i);
+                break;
+
+        }
+    }
+}
